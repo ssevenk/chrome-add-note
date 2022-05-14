3519ede91f2f7c97be92138977ad3dfcab6febf9
gaoyufeng.miller <gaoyufeng.miller@bytedance.com>
2022-05-14 21:22:22 +0800

    add note init

--- background.js
+++ background.js
@@ -1,21 +1,19 @@
-let color = '#3aa757';
-
+let Title = 'add note'
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
   chrome.contextMenus.create({
-    title: "change color",
-    contexts: ["all"],
-    id: "color"
+    title: Title,
+    contexts: ["selection"],
+    id: Title
   });
 });
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-  console.log({ info, tab})
-  if (info.menuItemId === "color") {
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, {from: "background.js"}, response => {
-          console.log("background -> content script infos have been sended");
-     }
-    )});
+  const { selectionText, menuItemId } = info
+  if (menuItemId === Title) {
+    chrome.storage.sync.get("myText", ({ myText }) => {
+      const newText =  myText + selectionText + '\n'
+      console.log({ myText, newText })
+      chrome.storage.sync.set({ myText: newText });
+    })
   }
 })
--- popup.js
+++ popup.js
@@ -1,15 +1,7 @@
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true }); // 获取当前页面
-  
-    chrome.scripting.executeScript({
-      // 对目标网页执行指定函数
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-  });
-  
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
-    });
-  }
+let textDom = document.getElementById("text");
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log({ request })
+	const { selectionText } = request;
+  textDom.innerHTML += '\n' + selectionText
+	sendResponse({});
+});
